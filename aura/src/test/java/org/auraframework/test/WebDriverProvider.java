7f4ed5d161c00ab33938e2bc0e014a719dd0f63a
Igor Shmulevich <ishmulevich@salesforce.com>
2014-11-09 22:26:48 -0800

    Added strict type checking to ServiceLoader methods

--- aura/src/test/java/org/auraframework/test/WebDriverProvider.java
+++ aura/src/test/java/org/auraframework/test/WebDriverProvider.java
@@ -15,6 +15,7 @@
  */
 package org.auraframework.test;
 
+import org.auraframework.ds.serviceloader.AuraServiceProvider;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -30,7 +31,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 // - to save port range: edit /etc/sysctl.conf and edit/add property for
 // net.ipv4.ip_local_port_range = 1025
 // 32000
-public interface WebDriverProvider {
+public interface WebDriverProvider extends AuraServiceProvider {
     String WEBDRIVER_SERVER_PROPERTY = "webdriver.remote.server";
     String REUSE_BROWSER_PROPERTY = "webdriver.reusebrowser";
     String BROWSER_TYPE_PROPERTY = "webdriver.browser.type";
